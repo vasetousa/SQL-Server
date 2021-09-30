a2ded40e94f830ab203f67557e6d8c18b7c922e5
Vasil <58910106+vasetousa@users.noreply.github.com>
2021-09-30 13:03:05 +0200

    new add

---
+++ Subqueries-and-JOINs/Sales Employees.sql
@@ -0,0 +1,15 @@
+USE [SoftUni]
+GO
+
+SELECT
+e.[EmployeeID],
+e.[FirstName],
+e.[LastName],
+d.[Name] AS DepartmentName
+
+FROM
+[Employees] AS e
+JOIN [Departments] as d ON d.[DepartmentID] = e.[DepartmentID]
+WHERE d.[Name] = 'Sales'
+ORDER BY e.[EmployeeID] ASC
+
