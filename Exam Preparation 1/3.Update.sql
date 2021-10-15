eb117cb96add97700737f71f0c7b50068183e462
Vasil <58910106+vasetousa@users.noreply.github.com>
2021-10-15 13:29:01 +0200

    added

---
+++ Exam Preparation 1/3.Update.sql
@@ -0,0 +1,5 @@
+USE Airport
+
+UPDATE Tickets SET Price = Price * 1.13 
+WHERE FlightId IN (SELECT Id FROM Flights WHERE Destination = 'Carlsbad')
+ 
---
+++ Exam Preparation 1/4.Delete.sql
@@ -0,0 +1,5 @@
+USE Airport
+
+DELETE FROM Tickets WHERE FlightId IN (SELECT Id FROM Flights WHERE Destination = 'Ayn Halagim');
+DELETE FROM Flights WHERE Destination = 'Ayn Halagim';
+ 
