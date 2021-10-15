b0458452c5db5ffeee01621bec637fdccde369d5
Vasil <58910106+vasetousa@users.noreply.github.com>
2021-10-15 17:58:59 +0200

    edit

--- Exam Preparation 1/11. Vacation.sql
+++ Exam Preparation 1/11. Vacation.sql
@@ -5,11 +5,11 @@ CREATE FUNCTION udf_CalculateTickets(@origin VARCHAR(50), @destination VARCHAR(5
    RETURNS VARCHAR(50) AS
 BEGIN
         IF (@peopleCount <= 0) RETURN 'Invalid people count!'
-        IF (NOT EXISTS (SELECT 1 FROM Flights WHERE Origin = @origin AND Destination = @destination))
+        IF (NOT EXISTS (SELECT 1 FROM [Flights] WHERE [Origin] = @origin AND [Destination] = @destination))
     RETURN 'Invalid flight!'
-    RETURN CONCAT('Total price ', (SELECT TOP(1) ts.Price FROM Tickets AS ts 
-      JOIN Flights AS f ON ts.FlightId = f.Id
-     WHERE f.Origin = @origin AND f.Destination = @destination) * @peopleCount)
+    RETURN CONCAT('Total price ', (SELECT TOP(1) ts.[Price] FROM [Tickets] AS ts 
+      JOIN [Flights] AS f ON ts.[FlightId] = f.[Id]
+     WHERE f.[Origin] = @origin AND f.Destination = @destination) * @peopleCount)
 END
  
  SELECT dbo.udf_CalculateTickets('Kolyshley','Rancabolang', 33)
